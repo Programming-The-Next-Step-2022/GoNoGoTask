9a6b8160692b0bbd93082e041989568180d7bab9
millapihlajamaki <pihlajamaki.milla@gmail.com>
2022-05-12 13:21:20 +0200

    new test function

--- gonogo/R/addition.R
+++ gonogo/R/addition.R
@@ -13,3 +13,20 @@
 add_5 <- function(x) {
   x + 5
 }
+
+#' Add 10 to any number
+#'
+#' @param x A number
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' x <- 3
+#' add_10(x)
+add_10 <- function(x) {
+  x + 10
+}
+
+
+
