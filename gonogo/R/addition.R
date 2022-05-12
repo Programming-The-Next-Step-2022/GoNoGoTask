1bb782959e72ab4baa8916ee21083a8a74f76343
millapihlajamaki <pihlajamaki.milla@gmail.com>
2022-05-12 13:17:52 +0200

    more practice

--- gonogo/R/addition.R
+++ gonogo/R/addition.R
@@ -1,5 +1,15 @@
 # A test, please ignore
 
+#' Add 5 to any number
+#'
+#' @param x A number
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' x <- 3
+#' add_5(x)
 add_5 <- function(x) {
   x + 5
 }
