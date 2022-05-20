2bb4872be24371013732867d3e2f2627989cd101
millapihlajamaki <pihlajamaki.milla@gmail.com>
2022-05-20 09:27:17 +0200

    Added description file gonogo.R, added a file for function validate_user_response and a test file for that

---
+++ gonogo/tests/testthat/test-validate_user_response.R
@@ -0,0 +1,19 @@
+library(gonogo)
+
+context('Validate_user_response functionality')
+
+test_that('validate_user_response return value depends on whether choice and choiceKeys match' {
+
+  random_letters <- sample(x = letters, size = 3, replace = FALSE)
+  choice_keys <- random_letters[1:2]
+  choice_match <- sample(choice_keys, 1)
+  choice_unmatch <- random_letters[3]
+
+  result1 <- validate_user_response(choice = choice_match, choiceKeys = choice_keys)
+  result2 <- validate_user_response(choice = choice_unmatch, choiceKeys = choice_keys)
+
+  expect_false(result1[1] == result2[1])
+
+})
+
+
