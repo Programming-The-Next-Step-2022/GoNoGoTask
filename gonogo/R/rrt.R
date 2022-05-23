2e3c5f4ce6522625d42f87be23e8301bb374229d
millapihlajamaki <pihlajamaki.milla@gmail.com>
2022-05-23 17:40:59 +0200

    Wrote the main gonogo() function

--- gonogo/R/rrt.R
+++ gonogo/R/rrt.R
@@ -2,32 +2,44 @@
 #'
 #'
 #'
-#' @param choiceKeys A character vector specifying the key(s) for which reaction time is recorded
+#' @param choiceKeys A character vector specifying the choice key(s) for which the reaction time is recorded
 #'
-#' @return Response (key pressed) and reaction time in seconds
+#' @param interval A numeric vector of length 1 specifying the length of each trial in seconds (default is 1)
+#'
+#' @return Response (either choice key pressed, or NA) and reaction time in seconds (NA if choice key not pressed)
 #' @export
 #'
 #' @examples
-rrt <- function(choiceKeys=c(" ")) {
-
+rrt <- function(choiceKeys=c(" "), interval = 0.6) {
   dynamic_readline <- function() {
+    # Create a counter variable that breaks the loop
+    x <- 0
     while (rstudioapi::isAvailable()) {
       input <- rstudioapi::getConsoleEditorContext()$contents
-
+      # Increase counter variable
+      x <- x + 1
       if (input != "") {
         rstudioapi::sendToConsole("", execute = FALSE)
         return(input)
       }
+      if (x == 5) {
+        return("none")
+      }
     }
     readline()
   }
-
+  o_rt <- Sys.time()
   repeat {
-    rt <- system.time({
       choice <- dynamic_readline()
-    })[3]
-    if (choice %in% choiceKeys) break
+    if (choice %in% choiceKeys){
+      o_rt <- Sys.time() - o_rt
+      break
+    }
+    elapsed_time <- Sys.time() - o_rt
+    if (elapsed_time > interval) {
+      o_rt <- 100
+      break
+    }
   }
-  setNames(c(choice, rt), c("response", "rt"))
+  setNames(c(choice, o_rt), c("response", "rt"))
 }
-
